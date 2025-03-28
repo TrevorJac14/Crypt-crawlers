01fb6d50613a0d1dce645097bb570d100efa3d25
Crobe182 <168012702+Crobe182@users.noreply.github.com>
2025-03-28 12:17:28 -0600

    start of game over screen

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -9,3 +9,11 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_give_up_pressed():
+	get_tree().quit()  # Quits the game
+
+
+func _on_try_again_pressed():
+	get_tree().reload_current_scene()  # Reloads the current game scene
